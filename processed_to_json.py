93b1a5fafc686d94e5206a89a389536b19d9246e
agercha <80985730+agercha@users.noreply.github.com>
2022-05-01 19:23:55 -0400

    Added creation of all_intros

--- processed_to_json.py
+++ processed_to_json.py
@@ -3,6 +3,7 @@ import os
 import json
 
 all_lines = []
+all_intros = []
 
 for file_name in os.listdir("edited_scripts"):
     f1_name = os.path.join("edited_scripts", file_name)
@@ -13,6 +14,7 @@ for file_name in os.listdir("edited_scripts"):
 
         total_chars = 0
         current_string = ""
+        first = True
         for (i, word) in enumerate(words):
             if total_chars + len(word) < 2048:
                 total_chars += len(word)
@@ -24,6 +26,14 @@ for file_name in os.listdir("edited_scripts"):
                 else: prompt = "End of "
                 curr_line["prompt"] = prompt + (file_name).strip(".txt")
                 curr_line["completion"] = current_string
+                all_lines.append(curr_line)
+                if first:
+                    first = False
+                    curr_intro = dict()
+                    curr_intro["prompt"] =  (file_name).strip(".txt")
+                    curr_intro["completion"] = current_string
+                    all_intros.append(curr_intro)
+
                 total_chars = len(word)
                 current_string = word
                 all_lines.append(curr_line)
@@ -31,3 +41,7 @@ for file_name in os.listdir("edited_scripts"):
 with open("all_data.json", "w") as f2:
     all_lines2 = '\n'.join(json.dumps(rec) for rec in all_lines)
     f2.write(all_lines2)
+
+with open("all_intros.json", "w") as f3:
+    all_intros2 = '\n'.join(json.dumps(rec) for rec in all_intros)
+    f3.write(all_intros2)
